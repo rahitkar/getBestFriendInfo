649eb930506ea16ce841555bed85fa8ca7bab50a
Step14 <step14@Step14s-MacBook-Pro.local>
2019-11-19 10:30:29 +0530

    the out put is now more readable

--- bestFriendsInfo.js
+++ bestFriendsInfo.js
@@ -1,17 +1,17 @@
-const info = [["Priyanshu","Sahil","Shanti","Ragini"],
-["Rashmi","Pallav","Pallavi","Pallabi"],
-["Sita","Janaka","Janaki","Rashmi"],
-["Ragini","Rajender","Tejinder","Priyanshu"],
-["Pallabi","Satyan","Subha","Priyanshu"]];
+const nameAndInfo = [["Priyanshu","Sahil","Shanti","Ragini"],
+  ["Rashmi","Pallav","Pallavi","Pallabi"],
+  ["Sita","Janaka","Janaki","Rashmi"],
+  ["Ragini","Rajender","Tejinder","Priyanshu"],
+  ["Pallabi","Satyan","Subha","Priyanshu"]];
 
-const makeStucture = function(previousList, infoList){
-  previousList[infoList[0]] = {father : infoList[1], mother : infoList[2], bestFriend : infoList[3]};
+const makeStucture = function(previousList, nameAndInfoList){
+  previousList[nameAndInfoList[0]] = {father : nameAndInfoList[1], mother : nameAndInfoList[2], bestFriend : nameAndInfoList[3]};
   return previousList;
 };
 
-const infoStuc = (info.reduce(makeStucture ,{}));
-//console.log(infoStuc);
+const newInfo = (nameAndInfo.reduce(makeStucture ,{}));
 const name = (process.argv).slice(2);
-console.log(infoStuc[infoStuc[name].bestFriend]);
-
-
+console.log("best friend's name = ", newInfo[name].bestFriend)
+console.log(newInfo[name].bestFriend,"'s father name = " , newInfo[newInfo[name].bestFriend].father);
+console.log(newInfo[name].bestFriend,"'s mother name = ", newInfo[newInfo[name].bestFriend].mother);
+console.log(newInfo[name].bestFriend,"'s best friend's name = ",newInfo[newInfo[name].bestFriend].bestFriend);
